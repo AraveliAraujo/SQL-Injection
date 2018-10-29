344a93d772725e4142bb90dc30b60e37ea802945
AraveliAraujo <aaraujo@vallegrande.edu.pe>
2018-10-29 09:45:03 -0500

    Terminado

--- SQL_Inyection@aaraujo/src/java/dao/LogeoD.java
+++ SQL_Inyection@aaraujo/src/java/dao/LogeoD.java
@@ -12,11 +12,11 @@ public class LogeoD extends Dao {
         ResultSet rs;
         LogeoM usu;
         try {
-//            String sql = "SELECT * FROM VW_LOGIN WHERE USEREMP LIKE '" + user + "' AND PSSEMP LIKE '" + pass + "'";
-            String sql = "SELECT * FROM VW_LOGIN WHERE USEREMP LIKE ? AND PSSEMP LIKE ? ";
+            String sql = "SELECT * FROM VW_LOGIN WHERE USEREMP LIKE '" + user + "' AND PSSEMP LIKE '" + pass + "'";
+//            String sql = "SELECT * FROM VW_LOGIN WHERE USEREMP LIKE ? AND PSSEMP LIKE ? ";
             PreparedStatement ps = this.getCn().prepareCall(sql);
-            ps.setString(1, user);
-            ps.setString(2, pass);
+//            ps.setString(1, user);
+//            ps.setString(2, pass);
             rs = ps.executeQuery();
             if (rs.next()) {
                 usu = new LogeoM();
